68e118e8f0e65b24585c1d76a0a6b308c54ba58b
Selvalingam Arumugam <g800746@ad.syniverse.com>
2016-09-22 10:10:50 +0530

    Notation notes auto save

--- application/views/user/notation.php
+++ application/views/user/notation.php
@@ -244,54 +244,9 @@
 												</td>
 												<td><input type="text" data-type="1" name="listCaseName[]" id="listCaseName_1" class="form-control autocomplete_casename" autocomplete="off"/></td>
 												<td><input type="text" data-type="1" name="citationNumber[]" id="citationNumber_1" class="form-control autocomplete_citation" autocomplete="off"/></td>
-												<td><!--<textarea  name="note[]" id="note_1" rows="1" class="form-control" style="height: auto;"></textarea> -->
-<textarea rows="1" style="height:1em;" id="text"></textarea>
+												<td><textarea  name="note[]" id="note_1" rows="1" class="form-control" style="height: auto;"></textarea>
 												</td>
 
-												<script type="text/javascript">
-													
-													var observe;
-													if (window.attachEvent) {
-													    observe = function (element, event, handler) {
-													        element.attachEvent('on'+event, handler);
-													    };
-													}
-													else {
-													    observe = function (element, event, handler) {
-													        element.addEventListener(event, handler, false);
-													    };
-													}
-													function init () {
-													    var text = document.getElementById('text');
-													    function resize () {
-													        text.style.height = 'auto';
-													        text.style.height = text.scrollHeight+'px';
-													    }
-													    /* 0-timeout to get the already changed text */
-													    function delayedResize () {
-													        window.setTimeout(resize, 0);
-													    }
-													    observe(text, 'change',  resize);
-													    observe(text, 'cut',     delayedResize);
-													    observe(text, 'paste',   delayedResize);
-													    observe(text, 'drop',    delayedResize);
-													    observe(text, 'keydown', delayedResize);
-
-													    text.focus();
-													    text.select();
-													    resize();
-													}
-													init();
-												</script>
-												<style type="text/css">
-													textarea {
-													    border: 0 none white;
-													    overflow: hidden;
-													    padding: 0;
-													    outline: none;
-													    background-color: #D0D0D0;
-													}
-												</style>
 
 											</tr>
 										</tbody>
